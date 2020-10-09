894c4292e8d0804b7af41c6b06bc03c90de06295
Endrew_Local <Endrew_Local@MONSTRO021>
2020-10-09 09:36:24 -0300

    ETLS

---
+++ POWERBI-SQL/POWERBI-SQL/COMBO.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[COMBO]
+(
+	[CODIGO_OFERTA] INT NOT NULL PRIMARY KEY, 
+    [leadlovers] NCHAR(10) NULL, 
+    [whatslovers] NCHAR(10) NULL, 
+    [CopyMaker] NCHAR(10) NULL, 
+    [99Webinar] NCHAR(10) NULL, 
+    [adzzy] NCHAR(10) NULL, 
+    [PageLovers] NCHAR(10) NULL, 
+    [VERIFICADOR] NCHAR(10) NULL
+)
